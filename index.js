ed129c75f7a4302f068fd71b336aa1cfc120169a
Enzovidal10 <enzovidal2001@gmail.com>
2022-10-06 16:15:46 -0300

    nuevo menu hamburguesa

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 
-//Volver hacia arriba
+//Función Menu hamburguesa
+function hamburgerMenu(panelBtn,panel){
+
+    const d=document;
+
+    d.addEventListener('click',(e)=>{
+        if(e.target.matches(panelBtn)){
+            d.querySelector(panel).classList.toggle("active");
+        }
+    })
+}
+
+//Parametros de la funcion cargdos al inicio de página
+document.addEventListener("DOMContentLoaded", (e)=>{
+    hamburgerMenu(".btn-hamburger", ".hamburger");
+})
+
+
+//Volver hacia arriba al clickear boton del footer
 let btn=document.querySelectorAll(".btn")
 
 btn.onclick=()=>{
